5c0aef4dee4f31db8a7ccae5e4992eda8b555f76
Muhammad Gohar Khan <Gohar.Khan@probaseapps.com>
2021-06-29 16:44:47 +0500

    layout changes

--- src/app/modules/people/organization-employee/header/employee-profile/header/employees/performance-history-graph/performance-history-graph.component.ts
+++ src/app/modules/people/organization-employee/header/employee-profile/header/employees/performance-history-graph/performance-history-graph.component.ts
@@ -1,11 +1,16 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  AfterViewInit,
+} from '@angular/core';
 import * as Highcharts from 'highcharts'; // load core
 import HC_more from 'highcharts/highcharts-more'; // load highcharts-more
-import { HttpClient } from "@angular/common/http";
+import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
-import { AllServicesService } from 'src/app/services/all-services.service'
+import { AllServicesService } from 'src/app/services/all-services.service';
 HC_more(Highcharts);
-
 
 interface ExtendedPointOptionsObject extends Highcharts.PointOptionsObject {
   country: string;
@@ -13,17 +18,15 @@ interface ExtendedPointOptionsObject extends Highcharts.PointOptionsObject {
 
 interface BubbleChart {
   data: Array<any>;
-  color: string
-
+  color: string;
 }
 
 @Component({
   selector: 'anms-performance-history-graph',
   templateUrl: './performance-history-graph.component.html',
-  styleUrls: ['./performance-history-graph.component.scss']
+  styleUrls: ['./performance-history-graph.component.scss'],
 })
 export class PerformanceHistoryGraphComponent implements OnInit {
-
   performanceData: any = [];
   chartData: any = [];
   reward: any = [];
@@ -36,7 +39,11 @@ export class PerformanceHistoryGraphComponent implements OnInit {
   training: any = [];
   @ViewChild('bubbleChartContainer') bubbleChartContainer: ElementRef;
   data: any = [];
-  constructor(private httpClient: HttpClient, private allServicesService: AllServicesService, private route: ActivatedRoute) { }
+  constructor(
+    private httpClient: HttpClient,
+    private allServicesService: AllServicesService,
+    private route: ActivatedRoute
+  ) {}
   ngOnInit() {
     let that = this;
     this.queryParams = this.route.snapshot.queryParams;
@@ -44,33 +51,53 @@ export class PerformanceHistoryGraphComponent implements OnInit {
 
   ngAfterViewInit() {
     let that = this;
-    that.allServicesService.getPerformance().subscribe(data => {
+    that.allServicesService.getPerformance().subscribe((data) => {
       that.performanceData = (data as any).data;
-      var employeeData = that.performanceData.find((x: any) => x.employeeId == that.queryParams.employeeId);
+      var employeeData = that.performanceData.find(
+        (x: any) => x.employeeId == that.queryParams.employeeId
+      );
       if (employeeData && employeeData.values) {
-        const grouped = this.groupBy(employeeData.values, (d: any) => d.pref_type);
+        const grouped = this.groupBy(
+          employeeData.values,
+          (d: any) => d.pref_type
+        );
 
         for (var entry of grouped.entries()) {
           this.fullData.push({
             name: entry[0],
             type: 'bubble',
-            data: this.loadPerformanceData(entry[1], entry[0])
-          })
+            data: this.loadPerformanceData(entry[1], entry[0]),
+          });
         }
-        var rewards: BubbleChart = { data: this.fullData[0].data, color: this.fullData[0].data[0].color }
-        var appericiation: BubbleChart = { data: this.fullData[1].data, color: this.fullData[1].data[0].color }
-        var warning: BubbleChart = { data: this.fullData[2].data, color: this.fullData[2].data[0].color }
-        var training: BubbleChart = { data: this.fullData[3].data, color: this.fullData[3].data[0].color }
+        var rewards: BubbleChart = {
+          data: this.fullData[0].data,
+          color: this.fullData[0].data[0].color,
+        };
+        var appericiation: BubbleChart = {
+          data: this.fullData[1].data,
+          color: this.fullData[1].data[0].color,
+        };
+        var warning: BubbleChart = {
+          data: this.fullData[2].data,
+          color: this.fullData[2].data[0].color,
+        };
+        var training: BubbleChart = {
+          data: this.fullData[3].data,
+          color: this.fullData[3].data[0].color,
+        };
         that.generatePerformance(rewards, appericiation, warning, training);
-      }
-      else {
+      } else {
         var emp_rewards = { data: [] as string[], color: '' };
         var emp_appericiation = { data: [] as string[], color: '' };
         var emp_warning = { data: [] as string[], color: '' };
         var emp_training = { data: [] as string[], color: '' };
-        that.generatePerformance(emp_rewards, emp_appericiation, emp_warning, emp_training);
+        that.generatePerformance(
+          emp_rewards,
+          emp_appericiation,
+          emp_warning,
+          emp_training
+        );
       }
-
 
       // that.data1 = data.filter((x: any) => x.employeeId == this.queryParams.employeeId);
     });
@@ -97,10 +124,20 @@ export class PerformanceHistoryGraphComponent implements OnInit {
       if (pData[j].pref_type == pref_type) {
         data.push({
           x: parseInt(pData[j].year),
-          y: pData[j].pref_given_user_name == 'hr' ? 1 : pData[j].pref_given_user_name == 'lm' ? 0 : 2,
-          z: pData[j].pref_level == 'min' ? 40 : pData[j].pref_level == 'mid' ? 65 : 80,
+          y:
+            pData[j].pref_given_user_name == 'hr'
+              ? 1
+              : pData[j].pref_given_user_name == 'lm'
+              ? 0
+              : 2,
+          z:
+            pData[j].pref_level == 'min'
+              ? 40
+              : pData[j].pref_level == 'mid'
+              ? 65
+              : 80,
           name: this.medals(pData[j].pref_type),
-          color: pData[j].color
+          color: pData[j].color,
         });
       }
     }
@@ -112,29 +149,34 @@ export class PerformanceHistoryGraphComponent implements OnInit {
     let that = this;
     switch (pref_type) {
       case 'pref_rewards':
-        return 'R'
+        return 'R';
       case 'pref_appericiation':
-        return 'A'
+        return 'A';
       case 'pref_warning':
-        return 'W'
+        return 'W';
       case 'pref_training':
-        return 'T'
+        return 'T';
       default:
         return '';
     }
   }
 
-  generatePerformance(rewards: BubbleChart, appericiation: BubbleChart, warning: BubbleChart, training: BubbleChart) {
+  generatePerformance(
+    rewards: BubbleChart,
+    appericiation: BubbleChart,
+    warning: BubbleChart,
+    training: BubbleChart
+  ) {
     let that = this;
     const options: Highcharts.Options = {
       chart: {
         backgroundColor: '#F2F3F4',
         type: 'bubble',
         plotBorderWidth: 0,
-        zoomType: 'xy'
+        zoomType: 'xy',
       },
       credits: {
-        enabled: false
+        enabled: false,
       },
       legend: {
         symbolPadding: 0,
@@ -145,25 +187,33 @@ export class PerformanceHistoryGraphComponent implements OnInit {
         labelFormatter: function () {
           switch (this.name) {
             case 'Performance Rewards':
-              return '<span  style="background: ' + this.options.color + ' ;color: white;padding: 20px 20px;font-size:13px;font-weight: bold">' +
+              return (
+                '<span  class="btn btn-xs btn-width btn-rounded btn-success rounded-pill">' +
                 this.name +
                 '</span>'
+              );
             case 'Letter of Appreciation':
-              return '<span  style="background: ' + this.options.color + ' ;color: white;padding: 20px 20px;font-size:13px;font-weight: bold">' +
+              return (
+                '<span  class="btn btn-xs btn-width btn-rounded btn-light rounded-pill">' +
                 this.name +
                 '</span>'
+              );
             case 'Warning Letter':
-              return '<span  style="background: ' + this.options.color + ' ;color: white;padding: 20px 20px;font-size:13px;font-weight: bold">' +
+              return (
+                '<span  class="btn btn-xs btn-width btn-rounded btn-warning rounded-pill">' +
                 this.name +
                 '</span>'
+              );
             case 'Training':
-              return '<span  style="background: ' + this.options.color + ' ;color: white;padding: 20px 20px;font-size:13px;font-weight: bold">' +
+              return (
+                '<span  class="btn btn-xs btn-width btn-rounded btn-info rounded-pill">' +
                 this.name +
                 '</span>'
+              );
             default:
               return '';
           }
-        }
+        },
       },
 
       title: {
@@ -173,14 +223,15 @@ export class PerformanceHistoryGraphComponent implements OnInit {
           color: '#45C23D',
           // 'background-color': '#9BE997',
           // 'padding': '28px 333px 10px 250px',
-          fontWeight: 'bold'
-        }
+          fontWeight: 'bold',
+        },
       },
 
       accessibility: {
         point: {
-          valueDescriptionFormat: '{index}. {point.name}, fat: {point.x}g, sugar: {point.y}g, obesity: {point.z}%.'
-        }
+          valueDescriptionFormat:
+            '{index}. {point.name}, fat: {point.x}g, sugar: {point.y}g, obesity: {point.z}%.',
+        },
       },
 
       xAxis: {
@@ -190,21 +241,33 @@ export class PerformanceHistoryGraphComponent implements OnInit {
           style: {
             color: '#3498DB',
             'background-color': 'white',
-            'padding': '4px',
+            padding: '4px',
             'box-shadow': '0px 1px',
-            'font-weight': 'bold'
-          }
+            'font-weight': 'bold',
+          },
         },
         lineColor: '#40A6DC',
         lineWidth: 3,
-        categories: ['2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020'],
+        categories: [
+          '2010',
+          '2011',
+          '2012',
+          '2013',
+          '2014',
+          '2015',
+          '2016',
+          '2017',
+          '2018',
+          '2019',
+          '2020',
+        ],
         gridLineWidth: 0,
         title: {
-          text: ''
+          text: '',
         },
         accessibility: {
-          rangeDescription: 'Range: 60 to 100 grams.'
-        }
+          rangeDescription: 'Range: 60 to 100 grams.',
+        },
       },
 
       yAxis: {
@@ -213,10 +276,10 @@ export class PerformanceHistoryGraphComponent implements OnInit {
           style: {
             color: '#3498DB',
             'background-color': 'white',
-            'padding': '4px',
+            padding: '4px',
             'box-shadow': '0px 1px',
-            'font-weight': 'bold'
-          }
+            'font-weight': 'bold',
+          },
         },
         categories: ['LM', 'HR', 'Managment', 'Employee'],
         gridLineWidth: 0,
@@ -225,81 +288,104 @@ export class PerformanceHistoryGraphComponent implements OnInit {
         lineColor: '#40A6DC',
         lineWidth: 3,
         title: {
-          text: ''
+          text: '',
         },
         maxPadding: 0.2,
         accessibility: {
-          rangeDescription: 'Range: 0 to 160 grams.'
-        }
+          rangeDescription: 'Range: 0 to 160 grams.',
+        },
       },
       tooltip: {
         useHTML: true,
         formatter: function () {
           return `
           <tr><th>Year</th><td> ${this.x}</td></tr><br />
-          <tr><th>Role:</th><td> ${this.y == 0 ? "LM" : this.y == 1 ? "HR" : "Managment"}</td></tr><br />
+          <tr><th>Role:</th><td> ${
+            this.y == 0 ? 'LM' : this.y == 1 ? 'HR' : 'Managment'
+          }</td></tr><br />
           <tr><th>Performance: </th><td>${this.color}</td></tr></table>`;
-        }
+        },
       },
 
       plotOptions: {
         bubble: {
           minSize: 3,
-          maxSize: 50
+          maxSize: 50,
         },
         series: {
-
           dataLabels: {
             enabled: true,
-            format: '{point.name}'
+            format: '{point.name}',
           },
-
-        }
+        },
       },
       series: [
         {
-          type: "bubble",
-          name: "Performance Rewards",
+          type: 'bubble',
+          name: 'Performance Rewards',
           data: rewards.data,
-          color: rewards.color
+          color: rewards.color,
         },
         {
-          type: "bubble",
-          name: "Letter of Appreciation",
+          type: 'bubble',
+          name: 'Letter of Appreciation',
           data: appericiation.data,
-          color: appericiation.color
+          color: appericiation.color,
         },
         {
-          type: "bubble",
-          name: "Warning Letter",
+          type: 'bubble',
+          name: 'Warning Letter',
           data: warning.data,
-          color: warning.color
+          color: warning.color,
         },
         {
-          type: "bubble",
-          name: "Training",
+          type: 'bubble',
+          name: 'Training',
           data: training.data,
-          color: training.color
+          color: training.color,
         },
         {
-          type: "bubble", name: '', data: [{ x: 2012, y: 3, z: 0 }], showInLegend: false, color: '#F2F3F4', enableMouseTracking: false
+          type: 'bubble',
+          name: '',
+          data: [{ x: 2012, y: 3, z: 0 }],
+          showInLegend: false,
+          color: '#F2F3F4',
+          enableMouseTracking: false,
         },
         {
-          type: "bubble", name: '', data: [{ x: 2012, y: 2, z: 0 }], showInLegend: false, color: '#F2F3F4', enableMouseTracking: false
+          type: 'bubble',
+          name: '',
+          data: [{ x: 2012, y: 2, z: 0 }],
+          showInLegend: false,
+          color: '#F2F3F4',
+          enableMouseTracking: false,
         },
         {
-          type: "bubble", name: '', data: [{ x: 2012, y: 1, z: 0 }], showInLegend: false, color: '#F2F3F4', enableMouseTracking: false
+          type: 'bubble',
+          name: '',
+          data: [{ x: 2012, y: 1, z: 0 }],
+          showInLegend: false,
+          color: '#F2F3F4',
+          enableMouseTracking: false,
         },
         {
-          type: "bubble", name: '', data: [{ x: 2012, y: 0, z: 0, name: '' }], showInLegend: false, color: '#F2F3F4', enableMouseTracking: false
+          type: 'bubble',
+          name: '',
+          data: [{ x: 2012, y: 0, z: 0, name: '' }],
+          showInLegend: false,
+          color: '#F2F3F4',
+          enableMouseTracking: false,
         },
         {
-          type: "bubble", name: '', data: [{ x: 2020, y: 0, z: 0 }], showInLegend: false, color: '#F2F3F4', enableMouseTracking: false
+          type: 'bubble',
+          name: '',
+          data: [{ x: 2020, y: 0, z: 0 }],
+          showInLegend: false,
+          color: '#F2F3F4',
+          enableMouseTracking: false,
         },
-      ]
+      ],
     };
     Highcharts.chart(that.bubbleChartContainer.nativeElement, options);
   }
-
-
 }
