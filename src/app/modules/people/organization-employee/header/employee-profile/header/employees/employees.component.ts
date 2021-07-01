a952a9de4864e9e638bb903d628414e823061418
ammaher1 <ammar.ami007@gmail.com>
2021-07-01 12:59:48 +0500

    JSON Dynamically colors applied

--- src/app/modules/people/organization-employee/header/employee-profile/header/employees/employees.component.ts
+++ src/app/modules/people/organization-employee/header/employee-profile/header/employees/employees.component.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, NgModule, ViewChild, ChangeDetectionStrategy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { TransferStoryService } from '../../../../../../../services/transfer-story.service';
 @Component({
   selector: 'anms-employees',
   templateUrl: './employees.component.html',
