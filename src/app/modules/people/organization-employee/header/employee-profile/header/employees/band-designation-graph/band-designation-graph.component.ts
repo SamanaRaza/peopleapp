6f67c9a4bac3422f1c1924acae78ce0a692478b5
Muhammad Gohar Khan <Gohar.Khan@probaseapps.com>
2021-07-08 14:16:00 +0500

    layout changes

--- src/app/modules/people/organization-employee/header/employee-profile/header/employees/band-designation-graph/band-designation-graph.component.ts
+++ src/app/modules/people/organization-employee/header/employee-profile/header/employees/band-designation-graph/band-designation-graph.component.ts
@@ -141,14 +141,14 @@ export class BandDesignationGraphComponent
         labelFormatter: function () {
           if (this.name == 'Bands') {
             return (
-              '<button class="mat-focus-indicator info-btn mat-raised-button mat-button-base btn-width"> <span class="mat-button-wrapper">' +
+              '<button class="mat-focus-indicator band-btn mat-raised-button mat-button-base btn-width"> <span class="mat-button-wrapper">' +
               this.name +
               '</span></button>'
             );
           } else this.name == 'Designations';
           {
             return (
-              '<button class="mat-focus-indicator danger-btn mat-raised-button mat-button-base btn-width"> <span class="mat-button-wrapper">' +
+              '<button class="mat-focus-indicator info-btn mat-raised-button mat-button-base btn-width"> <span class="mat-button-wrapper">' +
               this.name +
               '</span></button>'
             );
